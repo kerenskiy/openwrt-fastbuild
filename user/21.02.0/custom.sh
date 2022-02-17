775329d969f49e07ce2ea48e7b903629754f1b66
kerenskiy <admin@melamori.ru>
2022-02-17 13:29:13 +0300

    PEACG01 initial support

--- user/21.02.0/custom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "=============================================="
-rm ./package/feeds/packages/node
-rm ./package/feeds/packages/node-*
-./scripts/feeds install -a -p node
-./scripts/feeds install -a -p node
-make defconfig
-echo "=============================================="
